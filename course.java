4a72a6a713e1c43201dfcf54e4bd1b362ae19dd1
18SANJAY2005 <sb18042005@gmail.com>
2024-09-11 20:51:10 +0530

    Add files via upload

---
+++ course.java
@@ -0,0 +1,22 @@
+public class course{
+	public static void main(String[] args) {
+
+		String course = "English";
+
+		switch (course){
+		case "History":
+			System.out.println("enroll in History");
+			break;
+		case "Maths":
+			System.out.println("enroll in Maths");
+			break;
+		case "English":
+			System.out.println("enroll in English");
+		    break;
+		case "science":
+			System.out.println("enroll in science");
+			 break;
+		}
+		
+	}
+}
